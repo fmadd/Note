c595555d12923ff3d691d3a9478e30e4e4c21405
Anastasia <145687343+fmadd@users.noreply.github.com>
2024-06-11 21:38:07 +0300

    Delete FINALE directory

--- FINALE/build/libs/cryptopp-cmake/cryptopp-cmake/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Victoriya/Desktop/Study/FINALE/libs/cryptopp-cmake/cryptopp-cmake
-# Build directory: /mnt/c/Users/Victoriya/Desktop/Study/FINALE/build/libs/cryptopp-cmake/cryptopp-cmake
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
-subdirs("cryptopp")
